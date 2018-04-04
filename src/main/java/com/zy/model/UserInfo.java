ff2e42a833c339c7485f7d154141e985716fa099
xxz <37618292+superxxzxxz@users.noreply.github.com>
2018-04-04 16:51:13 +0800

    第一次提交

---
+++ src/main/java/com/zy/model/UserInfo.java
@@ -0,0 +1,33 @@
+package com.zy.model;
+
+public class UserInfo {
+    private Integer userId;
+
+    private Integer userAge;
+
+    private String userName;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(Integer userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+}
---
+++ src/test/java/com/zy/springbootzy/SpringbootzyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zy.springbootzy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootzyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
